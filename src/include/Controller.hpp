a03ab55a610569d6f23f120d8840e1ad2aeeb4b7
Marco <i449698@fhict.nl>
2023-03-23 14:04:35 +0100

    undersired var

--- src/include/Controller.hpp
+++ src/include/Controller.hpp
@@ -15,9 +15,8 @@ class RobotController
     private:
         float findOrientationToPoint(pos2d_t orientationCoords);
         Robot *_robot;
-        pos2d_t desiredGoal = pos2d(0,0);
         interrobot_t data;
-        float distTolerance = 0.5;
+        float distTolerance = 1;
 };
 
 #endif
